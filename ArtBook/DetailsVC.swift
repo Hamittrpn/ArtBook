978614430383b43a10aa2217d46140dc833470c5
Hamit Tırpan <hamittirpan@gmail.com>
2019-09-23 13:48:17 +0300

    first commit

---
+++ ArtBook/DetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsVC.swift
+//  ArtBook
+//
+//  Created by Hamit  Tırpan on 21.09.2019.
+//  Copyright © 2019 Hamit  Tırpan. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
