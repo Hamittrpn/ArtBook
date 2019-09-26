eb40cbb692c3c230b3a25e9e312d5ee2d36895ec
Hamit Tırpan <hamittirpan@gmail.com>
2019-09-26 18:12:57 +0300

    import CoreData, tableView

--- ArtBook/DetailsVC.swift
+++ ArtBook/DetailsVC.swift
@@ -7,24 +7,89 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsVC: UIViewController {
-
+class DetailsVC: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    var chosenPainting = ""
+    var chosenPaintingId : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        if chosenPainting != ""{
+            //CoreData
+        }
+        
+        //Recognizers
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+    }
+    @objc func selectImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
     }
-    */
-
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+    
+    // Eğer Storyboard içindeki componentleri viewControllera sürükleyip bıraktıktan sonra sarı ünlem hatası verir ise
+    // Main.storyboard'a sağ tık -> Open As -> SourceControl ile açıp ViewController satırına bakıp tag'ler içinde eksik olan
+    // alanları tamamlarsam sorun düzelecektir. 
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //Attributes
+        newPainting.setValue(nameText.text, forKey: "name")
+        newPainting.setValue(artistText.text, forKey: "artist")
+        
+        if let year = Int(yearText.text!){
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        // Yüklenen resmi %50 sıkıştırıp kaydetmek için
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        
+        newPainting.setValue(data, forKey: "image")
+        
+        // Save işlemini yapmak için "do-try-catch" kullanmalıyım.
+        do{
+            try context.save()
+                print("Saved")
+        } catch{
+            print("Error")
+        }
+        
+        // Tüm ViewController'ların mesajlaşıp haberleşebileceği bir yapı oluşturuyorum. newData diye bir mesaj gönderiyorum viewController'lara ve kullanmak istediğim yerlerde eğer bu mesajı alırsan şu fonksiyonu çalıştır gibi kullanabilirim.
+        NotificationCenter.default.post(name: NSNotification.Name("newData"), object: nil)
+        // Geri gitmek için...
+        self.navigationController?.popViewController(animated: true);
+        
+    }
+    
 }
--- ArtBook/ViewController.swift
+++ ArtBook/ViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    var selectedPainting = ""
+    var selectedPaintingId : UUID?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        getData()
     }
-
+    // Ekran her yüklendiğinde Dataları çekip,yenilemiş oluyorum.
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    
+    // Core Data'dan verileri çekeceğim fonksiyon
+    @objc func getData(){
+        
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate =  UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // Verileri çekme işlemi
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+        fetchRequest.returnsObjectsAsFaults = false // Core Data taraında cashe ile ilgili bir ayar.Büyük datasetlerde daha hızlı                                                  verileri almamı sağlar
+        do{
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject]{
+                if let name = result.value(forKey: "name") as? String{
+                    self.nameArray.append(name)
+                }
+                if let id = result.value(forKey: "id") as? UUID{
+                    self.idArray.append(id)
+                }
+                
+                // Yeni data geldi kendini güncelle!
+                self.tableView.reloadData()
+            }
+        }catch{
+            print("Error")
+        }
+    }
+    
+    @objc func addButtonClicked(){
+        selectedPainting = ""
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.chosenPainting = selectedPainting
+            destinationVC.chosenPaintingId = selectedPaintingId
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPainting = nameArray[indexPath.row]
+        selectedPaintingId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
 
 }
 
